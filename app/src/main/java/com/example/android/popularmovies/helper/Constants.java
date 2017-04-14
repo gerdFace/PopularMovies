3f51d4c8bec8b7653d0bea10d36b4b3e02d00459
gerdface <stevenagerdes@gmail.com>
2017-04-14 14:51:45 -0500

    added fragments for tablet functionality. updated UI.

--- app/src/main/java/com/example/android/popularmovies/helper/Constants.java
+++ app/src/main/java/com/example/android/popularmovies/helper/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String TRAILERS = "videos";
     public static final String MOVIES = "movie";
     public static final String REVIEWS = "reviews";
-    public final static String TMDB_KEY = "c7e6cb87a63c8cd9691cf319571b0581";
+    public final static String TMDB_KEY = "";
     public final static String KEY = "api_key";
     public static final String ERROR_CANNOT_CONNECT_TO_THE_MOVIE_DATABASE = "Error: cannot connect to the movie database";
     public final static String TMDB_BASE_URL = "https://api.themoviedb.org/3";
